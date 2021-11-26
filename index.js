6d81d9c43eb9efb40654ee4309390eee877cd9aa
Natalia Veríssimo <nattaliaverissimocosta@hotmail.com>
2021-11-26 17:50:35 -0300

    First commit with all files

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express")
+const app = express()   
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const Pergunta = require("./database/Pergunta")
+const Resposta = require("./database/Resposta")
+ 
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("conexão com banco")
+    })
+    .catch((erro)=>{
+        console.log(erro)
+    })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+ 
+app.get("/", (req, res) => {
+    //SELECT * ALL FROM perguntas
+    Pergunta.findAll({ raw : true, order:[
+        ['id', 'DESC'] //ASC = CRESCENTE DESC = decrescente
+    ]}).then(perguntas => {
+        res.render("index", {
+            perguntas: perguntas
+        }) 
+    })
+}) 
+
+app.get("/perguntar", (req, res) => {
+    res.render("perguntar")
+})
+
+app.post("/salvarpergunta", (req, res) => {
+    let titulo = req.body.titulo
+    let descricao = req.body.descricao
+
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() =>{
+        res.redirect("/")
+    })
+   })
+ 
+app.get("/pergunta/:id", (req, res) =>{
+    let id = req.params.id
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined) {
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order: [
+                    ['id', 'DESC']
+                ]
+            }).then(respostas =>{
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                })
+            })
+        }else{
+            res.redirect("/")
+        }
+    })
+})
+ 
+app.post("/responder", (req, res) => {
+    let corpo = req.body.corpo
+    let perguntaId = req.body.pergunta
+
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId)
+    })
+})
+
+app.listen(8089, () =>{
+    console.log("tudo certo")
+})
