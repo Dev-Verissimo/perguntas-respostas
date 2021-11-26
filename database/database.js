d91e919fa4d20d9238dccc4fdc2b3b71a1da8be5
Natalia Veríssimo <nattaliaverissimocosta@hotmail.com>
2021-11-26 19:29:36 -0300

    subindo o banco

--- database/database.js
+++ database/database.js
@@ -1,8 +1,5 @@
 const Sequelize = require("sequelize")
 
-const connection = new Sequelize("perguntasrespostas", "root", "1234", {
-    host: 'localhost',
-    dialect: 'mysql'
-})
+const connection = new Sequelize('mysql://bf1a9ec9e0a346:c9e62589@us-cdbr-east-04.cleardb.com/heroku_afd9d1bbde0e805?reconnect=true')
 
 module.exports = connection
